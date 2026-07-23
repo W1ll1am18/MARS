726e2f8b8bb48fc771932348031d96fcda35b845
William Lor <s4095329@student.rmit.edu.au>
2026-07-24 02:11:24 +1000

    Commented out user/security related files. Maybe in the future ill reimplement them

--- src/main/java/org/application/mars/Users/service/UserServiceImpl.java
+++ src/main/java/org/application/mars/Users/service/UserServiceImpl.java
@@ -1,48 +1,48 @@
-package org.application.mars.Users.service;
-
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.application.mars.Users.models.User;
-import org.application.mars.Users.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
-
-@Service
-@Transactional(rollbackOn =  Exception.class)
-@RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
-
-    @Autowired
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll(Sort.by("username"));
-    }
-
-    public User getUserById(UUID id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }
-
-    public User getUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }
-
-    public User createUser(User user) {
-        user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash()));
-        return userRepository.save(user);
-    }
-
-    public void delete(UUID id) {
-        userRepository.deleteById(id);
-    }
-
-    //Implement Upload Photo Later
-}
+//package org.application.mars.Users.service;
+//
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.application.mars.Users.models.User;
+//import org.application.mars.Users.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Service
+//@Transactional(rollbackOn =  Exception.class)
+//@RequiredArgsConstructor
+//public class UserServiceImpl implements UserService {
+//
+//    @Autowired
+//    private final PasswordEncoder passwordEncoder;
+//    private final UserRepository userRepository;
+//
+//    public List<User> getAllUsers() {
+//        return userRepository.findAll(Sort.by("username"));
+//    }
+//
+//    public User getUserById(UUID id) {
+//        return userRepository.findById(id)
+//                .orElseThrow(() -> new RuntimeException("User not found"));
+//    }
+//
+//    public User getUserByEmail(String email) {
+//        return userRepository.findByEmail(email)
+//                .orElseThrow(() -> new RuntimeException("User not found"));
+//    }
+//
+//    public User createUser(User user) {
+//        user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash()));
+//        return userRepository.save(user);
+//    }
+//
+//    public void delete(UUID id) {
+//        userRepository.deleteById(id);
+//    }
+//
+//    //Implement Upload Photo Later
+//}
